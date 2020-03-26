bb721d14334319964b9b14432361f53a46ae13c9
Edielson <edielsom@hotmail.com>
2020-03-26 09:14:44 -0400

    Final do Curso
    Tratamento de exceção e cancelamento de Thread.

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using ByteBank.Core.Model;
 using ByteBank.Core.Repository;
 using ByteBank.Core.Service;
-using ByteBank.View.Ultis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +14,7 @@ namespace ByteBank.View
     {
         private readonly ContaClienteRepository r_Repositorio;
         private readonly ContaClienteService r_Servico;
+        private CancellationTokenSource _cts;
 
         public MainWindow()
         {
@@ -26,17 +26,15 @@ namespace ByteBank.View
 
         private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
-
-
             BtnProcessar.IsEnabled = false;
+
+            _cts = new CancellationTokenSource();
 
             // Retorna o total de contas a serem processadas.
             var contas = r_Repositorio.GetContaClientes();
 
-
             // Obtêm o total de contas que serão processadas e coloca no progressBar.
             PgsProgresso.Maximum = contas.Count();
-
 
             // Limpa os controles.
             LimparView();
@@ -47,17 +45,40 @@ namespace ByteBank.View
             /* reescrever e entender como funciona uma classe Progress do .NET */
             //var byteBankProgress = new ByteBankProgress<String>(str => PgsProgresso.Value++);
 
+            BtnCancelar.IsEnabled = true;
+
             var progress = new Progress<String>(str => PgsProgresso.Value++);
-            // Efetua o processamento em uma thread.
-            var resultado = await ConsolidarContas(contas, progress);
 
-            // Obtêm o horário final do processamento.
-            var fim = DateTime.Now;
+            try
+            {
+                // Efetua o processamento em uma thread.
+                var resultado = await ConsolidarContas(contas, progress, _cts.Token);
 
-            // Atualiza os dados para o usuário.
-            AtualizarView(resultado, fim - inicio);
-            BtnProcessar.IsEnabled = true;
+                // Obtêm o horário final do processamento.
+                var fim = DateTime.Now;
 
+                // Atualiza os dados para o usuário.
+                AtualizarView(resultado, fim - inicio);
+            }
+            catch (OperationCanceledException)
+            {
+                TxtTempo.Text = "Operação cancelada pelo usuário";
+                PgsProgresso.Value = 0;
+            }
+            finally
+            {
+                BtnProcessar.IsEnabled = true;
+                BtnCancelar.IsEnabled = false;
+                
+            }
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            BtnCancelar.IsEnabled = false;
+
+            //Opção para o cancelamento da operação.
+            _cts.Cancel();
         }
 
         private void LimparView()
@@ -67,19 +88,31 @@ namespace ByteBank.View
             PgsProgresso.Value = 0;
         }
 
-        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<String> reportadorDeProgresso)
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas,
+            IProgress<String> reportadorDeProgresso, CancellationToken ct)
         {
 
             // Faz o processamento através de Thread.
             var tasks = contas.Select(conta =>
                  Task.Factory.StartNew(() =>
                  {
-                     var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta);
-          
+                     ////Lança a exceção de cancelamento, é neste momento que fazemos a rotina para desfazer o que foi executado.
+                     //if (ct.IsCancellationRequested)
+                     //    throw new OperationCanceledException(ct);
+
+                     //Caso não for desfazer a alteração e somente cancelar, utiliza a exceção do CancelationToken.
+                     ct.ThrowIfCancellationRequested(); //Verifica se a algum cancelamento e gera uma exceção.
+
+                     var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta,ct);
+
                      // Implementa a interface IProgress.
                      reportadorDeProgresso.Report(resultadoConsolidacao);
+
+                     if (ct.IsCancellationRequested)
+                         throw new OperationCanceledException(ct);
+
                      return resultadoConsolidacao;
-                 })
+                 },ct)
                  );
 
             return await Task.WhenAll(tasks);
@@ -93,6 +126,7 @@ namespace ByteBank.View
             LstResultados.ItemsSource = result;
             TxtTempo.Text = mensagem;
         }
+
 
     }
 }
